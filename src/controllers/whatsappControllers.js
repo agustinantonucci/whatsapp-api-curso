51948b72f4ce634212096b87e4b8ee4510bb97c9
agustinantonucci <agustinantonucci@gmail.com>
2022-11-24 09:33:47 -0300

    verificar token

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,12 +1,24 @@
 const VerifyToken = (req, res) => {
-    res.send("hola verifyToken");
-}
+  try {
+    let accessToken = "TASITUQUWOE1817IU5I1IAUSDF";
+    let token = req.query["hub.verify_token"];
+    let challenge = req.body["hub.challenge"];
+
+    if (challenge != null && token != null && token == accessToken) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
+    }
+  } catch (error) {
+    res.status(400).send();
+  }
+};
 
 const ReceivedMessage = (req, res) => {
-    res.send("hola received message");
-}
+  res.send("hola received message");
+};
 
 module.exports = {
-    VerifyToken,
-    ReceivedMessage
-}
+  VerifyToken,
+  ReceivedMessage,
+};
