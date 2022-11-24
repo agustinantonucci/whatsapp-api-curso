8677de59d2762882a878632ecaa7f33f1f63c314
agustinantonucci <agustinantonucci@gmail.com>
2022-11-24 09:21:34 -0300

    limpiando otra versión

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { v4 } = require("uuid");
 const apiRoutes = require("./src/routes/routes.js");
 
 const app = express();
@@ -9,18 +8,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/whatsapp", apiRoutes);
-
-// app.get("/api", (req, res) => {
-//   const path = `/api/item/${v4()}`;
-//   res.setHeader("Content-Type", "text/html");
-//   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-// });
-
-// app.get("/api/item/:slug", (req, res) => {
-//   const { slug } = req.params;
-//   res.end(`Item: ${slug}`);
-// });
 
 app.listen(PORT, () => {
   console.log("El puerto es: " + PORT);
